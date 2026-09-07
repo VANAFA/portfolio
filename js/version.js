1e596816fa88c9ba448299e716260bd6c17052ed
Vanafa <vfadel@udesa.edu.ar>
2026-09-07 19:21:28 -0300

    Stamp assets and version badge as 90cd1f0

--- js/version.js
+++ js/version.js
@@ -2,9 +2,9 @@
 // Updated by hand right after every push - if the badge you see doesn't match
 // the commit you were told about, it's a caching problem, not a missed commit.
 window.SITE_VERSION = {
-  hash: "405ecec",
-  updated: "2026-09-07 22:10 UTC",
-  url: "https://github.com/VANAFA/portfolio/commit/405ecec"
+  hash: "90cd1f0",
+  updated: "2026-09-07 22:21 UTC",
+  url: "https://github.com/VANAFA/portfolio/commit/90cd1f0"
 };
 
 (function () {
