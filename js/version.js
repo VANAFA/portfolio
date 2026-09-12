eec4aa0c6d6069d6f8ae5025f78ba296c3bde3d3
SuricataIngeniosa <nallibfadel@gmail.com>
2026-09-12 14:26:07 -0300

    Stamp assets and version badge as 1baff31

--- js/version.js
+++ js/version.js
@@ -2,9 +2,9 @@
 // Updated by hand right after every push - if the badge you see doesn't match
 // the commit you were told about, it's a caching problem, not a missed commit.
 window.SITE_VERSION = {
-  hash: "cdaa182",
-  updated: "2026-09-12 17:18 UTC",
-  url: "https://github.com/VANAFA/portfolio/commit/cdaa182"
+  hash: "1baff31",
+  updated: "2026-09-12 17:26 UTC",
+  url: "https://github.com/VANAFA/portfolio/commit/1baff31"
 };
 
 (function () {
